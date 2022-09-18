075c62dccad2a1f5fbd1cb700d6e35c3cf930eec
Jufeng Zhang <zjffun@gmail.com>
2022-09-18 16:27:18 +0800

    fix: dic count

--- packages/name-dic/src/front-end.ts
+++ packages/name-dic/src/front-end.ts
@@ -1,6 +1,8 @@
 import dic from "./front-end.dic";
 
+const dicWithCount = `${dic.split("\n").length}\n${dic}`;
+
 export default {
   aff: `SET UTF-8`,
-  dic,
+  dic: dicWithCount,
 };
--- packages/name-dic/src/programming-language.ts
+++ packages/name-dic/src/programming-language.ts
@@ -1,6 +1,8 @@
 import dic from "./programming-language.dic";
 
+const dicWithCount = `${dic.split("\n").length}\n${dic}`;
+
 export default {
   aff: `SET UTF-8`,
-  dic,
+  dic: dicWithCount,
 };
